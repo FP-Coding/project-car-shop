7bbdb4c7d9df7ab40d3827c4ca5fbf836978404b
Felipe Pinto <felipe14062000@gmail.com>
2023-03-22 19:40:11 -0300

    feat: atualização da interface ICarController

--- src/Interfaces/ICarController.ts
+++ src/Interfaces/ICarController.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 
 export default interface ICarController {
-  create(req: Request, res: Response, next: NextFunction): Promise<Response | undefined>
-  getById(req: Request, res: Response, next: NextFunction): Promise<Response | undefined>
-  getAll(req: Request, res: Response, next: NextFunction): Promise<Response | undefined>
+  create(req: Request, res: Response, next: NextFunction): Promise<Response | undefined>,
+  getById(req: Request, res: Response, next: NextFunction): Promise<Response | undefined>,
+  getAll(req: Request, res: Response, next: NextFunction): Promise<Response | undefined>,
+  update(req: Request, res: Response, next: NextFunction): Promise<Response | undefined>,
 } 
