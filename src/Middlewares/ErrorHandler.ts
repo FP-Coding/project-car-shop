2ca7b0f0a82b2c27c34d907610080ad84e5b8206
Felipe Pinto <felipe14062000@gmail.com>
2023-03-22 18:53:16 -0300

    feat: criação do middleware ErrorHandle

--- src/Middlewares/ErrorHandler.ts
+++ src/Middlewares/ErrorHandler.ts
@@ -1,8 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
+import IError from '../Interfaces/IError';
 
 class ErrorHandler { 
-  static handle(err: Error, _req: Request, res: Response, _next: NextFunction) {
-    return res.status(500).json({ message: err.message });
+  static handle(err: IError, _req: Request, res: Response, _next: NextFunction) {
+    return res.status(err.type || 500).json({ message: err.message });
   }
 }
 
