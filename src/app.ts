5aafbe9f2bb605ee393df6aab4c192d98e55edf7
Felipe Pinto <felipe14062000@gmail.com>
2023-03-22 17:14:18 -0300

    feat: configuração e uso das rotas no app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import ErrorHandler from './Middlewares/ErrorHandler';
+import carRoute from './Routes/Car.routes';
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+app.use('/cars', carRoute);
+
+app.use(ErrorHandler.handle);
 
 export default app;
