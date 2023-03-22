97fe0c91a5c79c5261bf3b2c4ad401b018bdd0b7
Felipe Pinto <felipe14062000@gmail.com>
2023-03-22 19:01:01 -0300

    feat: atualização da interface ICarService

--- src/Interfaces/ICarService.ts
+++ src/Interfaces/ICarService.ts
@@ -1,5 +1,7 @@
 import ICar from './ICar';
 
 export default interface ICarService {
-  create(CarInfo: ICar): Promise<ICar>
+  create(CarInfo: ICar): Promise<ICar>,
+  getById(id: string): Promise<ICar>,
+  getAll(): Promise<ICar[]>
 }
