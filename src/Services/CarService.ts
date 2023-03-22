b5ce9fd63d47aa2102441b3fadac201647b76bbf
Felipe Pinto <felipe14062000@gmail.com>
2023-03-22 19:07:16 -0300

    feat: atualização do CarService

--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -1,4 +1,7 @@
+import { isValidObjectId } from 'mongoose';
 import Car from '../Domains/Car';
+import NotFound from '../Errors/NotFound';
+import InvalidRequest from '../Errors/InvalidRequest';
 import ICar from '../Interfaces/ICar';
 import ICarService from '../Interfaces/ICarService';
 import CarModel from '../Models/CarModel';
@@ -15,6 +18,20 @@ class CarService implements ICarService {
     
     const createdCar = new Car(result).returnInfos();
     return createdCar;
+  }
+
+  async getById(id: string): Promise<ICar> {
+    if (!isValidObjectId(id)) throw new InvalidRequest('Invalid mongo id');
+    const result = await this._model.getById(id);
+    if (!result) throw new NotFound('Car not found');
+    const foundedCar = new Car(result).returnInfos();
+    return foundedCar;
+  }
+
+  async getAll(): Promise<ICar[]> {
+    const result = await this._model.getAll();
+    const cars = result.map((car) => new Car(car).returnInfos());
+    return cars;
   }
 }
 
