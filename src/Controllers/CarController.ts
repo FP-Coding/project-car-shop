6e2a01b62dd0ba11357b57c914803fe093067860
Felipe Pinto <felipe14062000@gmail.com>
2023-03-22 19:40:28 -0300

    feat: atualização da Class CarController

--- src/Controllers/CarController.ts
+++ src/Controllers/CarController.ts
@@ -38,6 +38,18 @@ class CarController implements ICarController {
       next(error);
     }
   }
+
+  async update(req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
+    try {
+      const { id } = req.params;
+      const { body } = req;
+      const carInfoToUpdate = new Car(body).returnInfos();
+      const updateCar = await this._service.update(id, carInfoToUpdate);
+      return res.status(200).json(updateCar);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export default CarController;
